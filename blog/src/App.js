efd379b33f2dcad368b419a34b382e54c9a0d0e5
zakariae_elhajri <zakriaelhajri@gmail.com>
2023-03-05 13:25:15 +0100

    edit Home.js and add simple Click Event (with parametre , without)

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,6 +1,5 @@
 import Navbar from './Navbar';
 import Home from './Home';
-
 function App() {
   return (
     <div className="App">
--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = () => {
+        console.log(' button clicked');
+    }
+    const handleClickAgain = (name) => {
+        console.log(' button 2 clicked'+name);
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click button</button>
+            <button onClick={() => handleClickAgain(' zakariae')}>Click button</button>
         </div>
      );
 }
