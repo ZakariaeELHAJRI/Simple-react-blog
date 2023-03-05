edd0dbc983260e046ae8904e17770b06f04a975e
zakariae_elhajri <zakriaelhajri@gmail.com>
2023-03-05 15:11:18 +0100

    create static array containe blogs and styling div[blog-preview] and for each click on this div show body of blog in div[blog-details]

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -1,19 +1,33 @@
 import { useState } from "react";
 const Home = () => {
-    const [name , setName]= useState('zakariae');
-    const handleClick = () => {
-        console.log(' button clicked');
-        setName('Zakariae Elhajri');
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'My new website lorem ipsum...', author: 'zakariae', id: 1 },
+        { title: 'Welcome party!', body: 'Welcome party lorem ipsum...', author: 'bilal', id: 2 },
+        { title: 'Web dev top tips', body: 'Web dev top tips lorem ipsum...', author: 'samir', id: 3 }
+    ]);
+    const [details, setDetails] = useState('');
+ 
    
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{name}</p>
-            <button onClick={handleClick}>Click button to change name</button>
-            
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id} onClick={()=>(
+                    setDetails(blog.body)  
+                )}>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+
+                </div>
+            ))}
+               <div className="blog-details">
+                    
+                    <p> { details}</p>
+
+                </div>
+                 
+           
         </div>
-     );
+    );
 }
  // useState hook to make a reactive value 
 export default Home;
