00e844df5db7d7d125816d925b3138ea0ed13a0f
zakariae_elhajri <zakriaelhajri@gmail.com>
2023-03-19 14:00:40 +0000

    Conditional Loading Message

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-   
+   const [isPending, setIsPending] = useState(true);
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -11,18 +11,23 @@ const Home = () => {
 
     //useEffect is a hook that runs after every change in changeUSer 
     useEffect(() => {
-       
-        fetch('http://localhost:8000/blogs')
+       setTimeout(() => {
+              fetch('http://localhost:8000/blogs')
             .then(res => {
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
-            })
+                setIsPending(false);
+            });
+
+       }, 2000);
+  
     }, []);
    //create a new external component to handle the blog list and use props to pass the data
     return ( 
         <div className="home">
+        {isPending && <div>Loading...</div>}
         {blogs && <BlogList  blogs={blogs} handleDelete={handleDelete}/>}
         </div>
     );
